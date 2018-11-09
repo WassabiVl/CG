a3f0293cbe9600a5cae239e24f33c95256755bc2
wassabivl <wassab.dietaube@gmail.com>
2018-11-09 16:00:18 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/framework.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wvl/Desktop/CG/OpenGLFramework-master/external/tinyobjloader-aa07206/tiny_obj_loader.cc" "/home/wvl/Desktop/CG/OpenGLFramework-master/cmake-build-debug/CMakeFiles/framework.dir/external/tinyobjloader-aa07206/tiny_obj_loader.cc.o"
+  "/home/wvl/Desktop/CG/OpenGLFramework-master/framework/source/application.cpp" "/home/wvl/Desktop/CG/OpenGLFramework-master/cmake-build-debug/CMakeFiles/framework.dir/framework/source/application.cpp.o"
+  "/home/wvl/Desktop/CG/OpenGLFramework-master/framework/source/model.cpp" "/home/wvl/Desktop/CG/OpenGLFramework-master/cmake-build-debug/CMakeFiles/framework.dir/framework/source/model.cpp.o"
+  "/home/wvl/Desktop/CG/OpenGLFramework-master/framework/source/model_loader.cpp" "/home/wvl/Desktop/CG/OpenGLFramework-master/cmake-build-debug/CMakeFiles/framework.dir/framework/source/model_loader.cpp.o"
+  "/home/wvl/Desktop/CG/OpenGLFramework-master/framework/source/shader_loader.cpp" "/home/wvl/Desktop/CG/OpenGLFramework-master/cmake-build-debug/CMakeFiles/framework.dir/framework/source/shader_loader.cpp.o"
+  "/home/wvl/Desktop/CG/OpenGLFramework-master/framework/source/texture_loader.cpp" "/home/wvl/Desktop/CG/OpenGLFramework-master/cmake-build-debug/CMakeFiles/framework.dir/framework/source/texture_loader.cpp.o"
+  "/home/wvl/Desktop/CG/OpenGLFramework-master/framework/source/utils.cpp" "/home/wvl/Desktop/CG/OpenGLFramework-master/cmake-build-debug/CMakeFiles/framework.dir/framework/source/utils.cpp.o"
+  "/home/wvl/Desktop/CG/OpenGLFramework-master/framework/source/window_handler.cpp" "/home/wvl/Desktop/CG/OpenGLFramework-master/cmake-build-debug/CMakeFiles/framework.dir/framework/source/window_handler.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLBINDING_STATIC_DEFINE"
+  "SYSTEM_LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/tinyobjloader-aa07206"
+  "../external/glfw-3.1.1/include"
+  "../framework/include"
+  "../application/include"
+  "../external/glbinding-2.1.1/source/glbinding/include"
+  "external/glbinding-2.1.1/source/glbinding/include"
+  "../external/glm-0.9.6.3"
+  "../external/stb_image-2.0.6"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/wvl/Desktop/CG/OpenGLFramework-master/cmake-build-debug/external/glbinding-2.1.1/source/glbinding/CMakeFiles/glbinding.dir/DependInfo.cmake"
+  "/home/wvl/Desktop/CG/OpenGLFramework-master/cmake-build-debug/external/glfw-3.1.1/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
